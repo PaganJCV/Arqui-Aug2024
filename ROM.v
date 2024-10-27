7c704055e35a5f4563d8554317fd0aa31c31758f
Jean Carlos Pagán Vega <jean.pagan5@upr.edu>
2024-10-27 19:19:31 -0400

    Fixed Commits

--- ROM.v
+++ ROM.v
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module ROM(
     input [7:0] address,  
     output reg [31:0] instruction  
@@ -9,16 +8,4 @@ module ROM(
       instruction = {Mem[address], Mem[address+1], Mem[address+2], Mem[address+3]};
     end
 
-=======
-module ROM(
-    input [7:0] address,  
-    output reg [31:0] instruction  
-);
-    reg [7:0] Mem [0:255];
-
-    always @(address) begin
-      instruction = {Mem[address], Mem[address+1], Mem[address+2], Mem[address+3]};
-    end
-
->>>>>>> 1db9e949b472e9b3ff5d4bed6418ec777fa0fb9b
 endmodule
