c65c8647ddb71b821cd8ca085936266b1bd02f2c
Joel Lopez <joel.lopez12@upr.edu>
2024-10-28 15:02:00 -0400

    Test Bench until COntrol Unit instantiation

--- ControlUnit_TB.v
+++ ControlUnit_TB.v
@@ -1,19 +1,112 @@
-module clock_gen(
-    output reg clk
-);
+module CU_tb;
+    reg clk, R, LE;
+    integer fi, code;
+    //PC 
+    reg [7:0] in_pc;
+    wire [7:0] out_pc;
 
-    initial begin
-        clk = 0;
-        #40 $finish;
+    //PC adder
+    reg [7:0] num;
+    wire [7:0] result;
 
-    end
+    //IF_ID 
+    reg [31:0] rom_instruction;
+    wire [31:0] instruction;
 
-    always begin
-        #1 clk = ~clk; // Toggle the clock every 2 time units
-    end
+    //Control Unit
+    reg [31:0] in_instruction;
+    wire [3:0] opcode;
+    wire AM,
+         S_enable,
+         load_instr,
+         RF_enable,
+         Size_enable,
+         RW_enable,
+         Enable_signal,
+         BL_instr,
+         B_instr;
 
-       always @(posedge clk) begin
-        $display("Clock has a positive edge at time %0t", $time);
-    end
+    //CU mux
+    reg SS;
+    reg [3:0] mux_opcode;
+    reg mux_AM,
+        mux_S_enable,
+        mux_load_instr,
+        mux_RF_enable,
+        mux_Size_enable,
+        mux_RW_enable,
+        mux_Enable_signal,
+        mux_BL_instr,
+        mux_B_instr;
+    wire [3:0] ID_opcode;
+    wire ID_AM,
+         ID_S_enable,
+         ID_load_instr,
+         ID_RF_enable,
+         ID_Size_enable,
+         ID_RW_enable,
+         ID_Enable_signal,
+         ID_BL_instr,
+         ID_B_instr;
+
+    //ID_EX
+    reg [3:0] in_ID_opcode;
+    reg   in_ID_AM,
+          in_ID_S_enable,
+          in_ID_load_instr,
+          in_ID_RF_enable,
+          in_ID_Size_enable,
+          in_ID_RW_enable,
+          in_ID_Enable_signal;
+    wire [3:0] EX_opcode;
+    wire  EX_AM,
+          EX_S_enable,
+          EX_load_instr,
+          EX_RF_enable,
+          EX_Size_enable,
+          EX_RW_enable,
+          EX_Enable_signal;
+    
+    //EX_MEM
+    reg   in_EX_load_instr,
+          in_EX_RF_enable,
+          in_EX_Size_enable,
+          in_EX_RW_enable,
+          in_EX_Enable_signal;
+    wire  MEM_load_instr,
+          MEM_RF_enable,
+          MEM_Size_enable,
+          MEM_RW_enable,
+          MEM_Enable_signal;
+    
+    //MEM_WB
+    reg in_MEM_RF_enable;
+    wire output reg WB_RF_enable;
+
+PC pc_tb(clk, R, LE, in_pc, out_pc);
+
+PC_adder adder_tb(num, result);
+
+IF_ID ifid(clk,R, rom_instruction, LE, instruction);
+
+Control_Unit cu_tb(in_instruction, opcode, 
+               AM,
+               S_enable,
+               load_instr,
+               RF_enable,
+               Size_enable,
+               RW_enable,
+               Enable_signal,
+               BL_instr,
+               B_instr);
+
+
+
+initial begin
+    clk = 0;
+    repeat (20) #2 clk = ~clk;
+end
+
+
 
 endmodule
